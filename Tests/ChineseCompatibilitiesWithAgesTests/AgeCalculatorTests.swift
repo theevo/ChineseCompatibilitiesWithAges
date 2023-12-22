d6b1fab79944bc1468dd006404b188443ea8fe8c
Theo Vora <theo.vora@gmail.com>
2023-12-22 17:38:11 -0600

    refactor: write helper to construct Date from string

--- Tests/ChineseCompatibilitiesWithAgesTests/AgeCalculatorTests.swift
+++ Tests/ChineseCompatibilitiesWithAgesTests/AgeCalculatorTests.swift
@@ -9,34 +9,18 @@ import XCTest
 @testable import ChineseCompatibilitiesWithAges
 
 final class AgeCalculatorTests: XCTestCase {
-    var dec222023: Date {
-        let formatter = DateFormatter.inUTCTimeZone(dateFormat: "MM-dd-yyyy")
-        guard let swiftDate = formatter.date(from: "12-22-2023") else {
-            fatalError()
-        }
-        return swiftDate
-    }
-    
-    var jan012023: Date {
-        let formatter = DateFormatter.inUTCTimeZone(dateFormat: "MM-dd-yyyy")
-        guard let swiftDate = formatter.date(from: "01-01-2023") else {
-            fatalError()
-        }
-        return swiftDate
-    }
-    
     func test_birthday_11_26_1978_isAge45() throws {
-        let sut = try AgeCalculator(birthday: "11-26-1978", today: dec222023)
+        let sut = try AgeCalculator(birthday: "11-26-1978", today: date("12-22-2023"))
         XCTAssertEqual(sut.age, 45)
     }
     
     func test_birthday_10_31_1947_isPig() throws {
-        let sut = try AgeCalculator(birthday: "10-31-1947", today: dec222023)
+        let sut = try AgeCalculator(birthday: "10-31-1947", today: date("12-22-2023"))
         XCTAssertEqual(sut.age, 76)
     }
     
     func test_birthday_11_26_1978_isAge44() throws {
-        let sut = try AgeCalculator(birthday: "11-26-1978", today: jan012023)
+        let sut = try AgeCalculator(birthday: "11-26-1978", today: date("01-01-2023"))
         XCTAssertEqual(sut.age, 44)
     }
     
@@ -58,5 +42,15 @@ final class AgeCalculatorTests: XCTestCase {
         }
 
         XCTAssertTrue(true)
+    }
+    
+    // MARK: - Helpers
+    
+    func date(_ string: String) -> Date {
+        let formatter = DateFormatter.inUTCTimeZone(dateFormat: "MM-dd-yyyy")
+        guard let swiftDate = formatter.date(from: string) else {
+            fatalError()
+        }
+        return swiftDate
     }
 }
