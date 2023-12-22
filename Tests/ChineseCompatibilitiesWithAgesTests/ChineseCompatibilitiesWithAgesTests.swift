fc59bd6877bcff9cb731d0c5ee8ba90cdb3021bb
Theo Vora <theo.vora@gmail.com>
2023-12-22 14:14:27 -0600

    provide age

--- Tests/ChineseCompatibilitiesWithAgesTests/ChineseCompatibilitiesWithAgesTests.swift
+++ Tests/ChineseCompatibilitiesWithAgesTests/ChineseCompatibilitiesWithAgesTests.swift
@@ -6,4 +6,9 @@ final class ChineseCompatibilitiesWithAgesTests: XCTestCase {
         let sut = try ChineseCompatibilitiesWithAges(birthday: "11-26-1978")
         XCTAssertEqual(sut.animal, .Horse)
     }
+    
+    func test_birthday_11_26_1978_isAge45() throws {
+        let sut = try ChineseCompatibilitiesWithAges(birthday: "11-26-1978")
+        XCTAssertEqual(sut.age, 45)
+    }
 }
