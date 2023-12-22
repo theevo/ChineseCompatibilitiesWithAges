ce2e4a2ccfc39b9d51ce1bdfedf07d5a093b3851
Theo Vora <theo.vora@gmail.com>
2023-12-22 14:24:14 -0600

    add test case for Pig born in 1947

--- Tests/ChineseCompatibilitiesWithAgesTests/ChineseCompatibilitiesWithAgesTests.swift
+++ Tests/ChineseCompatibilitiesWithAgesTests/ChineseCompatibilitiesWithAgesTests.swift
@@ -2,13 +2,15 @@ import XCTest
 import ChineseCompatibilitiesWithAges
 
 final class ChineseCompatibilitiesWithAgesTests: XCTestCase {
-    func test_birthday_11_26_1978_isHorse() throws {
+    func test_birthday_11_26_1978_isHorse_isAge45() throws {
         let sut = try ChineseCompatibilitiesWithAges(birthday: "11-26-1978")
         XCTAssertEqual(sut.animal, .Horse)
+        XCTAssertEqual(sut.age, 45)
     }
     
-    func test_birthday_11_26_1978_isAge45() throws {
-        let sut = try ChineseCompatibilitiesWithAges(birthday: "11-26-1978")
-        XCTAssertEqual(sut.age, 45)
+    func test_birthday_10_31_1947_isPig_isAge75() throws {
+        let sut = try ChineseCompatibilitiesWithAges(birthday: "10-31-1947")
+        XCTAssertEqual(sut.animal, .Pig)
+        XCTAssertEqual(sut.age, 76)
     }
 }
