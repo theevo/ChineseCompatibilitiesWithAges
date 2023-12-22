d8571b1f131ff372d851e101dcbb38401fde15f2
Theo Vora <theo.vora@gmail.com>
2023-12-22 14:20:06 -0600

    refactor: create new file for AgeCalculator

---
+++ Sources/ChineseCompatibilitiesWithAges/AgeCalculator.swift
@@ -0,0 +1,40 @@
+//
+//  AgeCalculator.swift
+//
+//
+//  Created by Theo Vora on 12/22/23.
+//
+
+import Foundation
+
+internal struct AgeCalculator {
+    var age: Int
+    
+    init(birthday: String) throws {
+        let formatter = DateFormatter.inUTCTimeZone(dateFormat: "MM-dd-yyyy")
+        guard let swiftDate = formatter.date(from: birthday) else {
+            throw Error.invalidBirthdayInputString(date: birthday)
+        }
+        
+        let gregorianCalendar = Calendar(identifier: .gregorian)
+        let today = Date()
+        let dateComponents = gregorianCalendar.dateComponents([.year], from: swiftDate, to: today)
+        
+        guard let years = dateComponents.year else { throw Error.invalidBirthdayInputString(date: birthday) }
+        
+        self.age = years
+    }
+}
+
+extension AgeCalculator {
+    enum Error: LocalizedError {
+        case invalidBirthdayInputString(date: String)
+        
+        var description: String {
+            switch self {
+            case .invalidBirthdayInputString(let date):
+                return "Invalid birthdate: \(date). Format must be MM-DD-YYYY."
+            }
+        }
+    }
+}
